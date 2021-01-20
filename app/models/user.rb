c60588e42233628306b75a5e24515b9c29504688
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-01-20 18:59:20 +0900

    userモデルのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password,          format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i, message: 'は半角英数字どちらも含む8文字以上で入力してください' }
+  with_options presence: true do
+    validates :nickname
+    validates :last_name,       format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください' }
+    validates :first_name,      format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください' }
+  end
 end
