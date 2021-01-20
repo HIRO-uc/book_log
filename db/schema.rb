49da245e8097c21c0cc1ccd703932cba7b63ddf6
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-01-20 22:12:12 +0900

    booksテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_20_092346) do
+ActiveRecord::Schema.define(version: 2021_01_20_112416) do
+
+  create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.string "authors"
+    t.string "published_date"
+    t.string "image_link"
+    t.integer "isbn"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_01_20_092346) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "books", "users"
 end
