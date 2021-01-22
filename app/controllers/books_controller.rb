35bb27eb716b08587f9f2442150d503d832d576e
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-01-22 20:58:34 +0900

    Update books_controller.rb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,13 +7,27 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
+    @book = Book.new(new_book_params)
     @book.save
     redirect_to books_search_path(keyword: params[:keyword], page: params[:page])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.update(edit_book_params)
+    redirect_to user_path(current_user)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
   private
-  def book_params
+  def new_book_params
     params.permit(:title, :authors, :publisher, :published_date, :description, :image_link, :info_link, :isbn, :status_id).merge(user_id: current_user.id)
+  end
+
+  def edit_book_params
+    params.require(:book).permit(:status_id)
   end
 end
