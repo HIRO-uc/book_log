26fa9ca70bfa72d84a4600db5dfc3fa955810823
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-01-22 21:38:16 +0900

    自分以外のユーザーに対する機能制限の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,20 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :current_path_user
   before_action :move_to_top
 
   def show
-    @books = current_user.books
+    @books = @user.books
+  end
+
+  private
+  def current_path_user
+    @user = User.find(params[:id])
   end
 
   def move_to_top
-    redirect_to root_path if current_user.id != params[:id]
+    if @user.id != current_user.id
+      redirect_to root_path 
+    end
   end
 end
