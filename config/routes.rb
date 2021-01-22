edb9dcd15bb23fa25f998ae5ee94c837b0768d86
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-01-22 20:50:41 +0900

    アイコン編集と本の編集ページのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#top'
   get 'books/search'
-  resources :books, only: [:create]
-  resources :users, only: :show
+  resources :books, only: [:create, :show, :update] 
+  resources :users, only: :show do
+    get 'icon_edit'
+  end
 end
