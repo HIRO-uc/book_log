605655c726a6c22d18739e27a8009a518035ea2a
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-01-24 18:22:33 +0900

    userモデルのバリデーションとアソシエーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books
+  has_one :profile
+  accepts_nested_attributes_for :profile
   validates :password,          format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i, message: 'は半角英数字どちらも含む8文字以上で入力してください' }
   with_options presence: true do
-    validates :nickname
     validates :last_name,       format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください' }
     validates :first_name,      format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください' }
   end
