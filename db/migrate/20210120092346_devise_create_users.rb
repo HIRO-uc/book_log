cc908af9e3f13f84436180fd149d202286cb36ef
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-01-24 18:22:48 +0900

    マイグレーションファイルの修正

--- db/migrate/20210120092346_devise_create_users.rb
+++ db/migrate/20210120092346_devise_create_users.rb
@@ -8,7 +8,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :encrypted_password, null: false, default: ""
       t.string  :last_name
       t.string  :first_name
-      t.string  :nickname
 
       ## Recoverable
       t.string   :reset_password_token
---
+++ db/migrate/20210124073704_create_profiles.rb
@@ -0,0 +1,13 @@
+class CreateProfiles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :profiles do |t|
+
+      t.string     :nickname
+      t.string     :image, default: 'user-default.png'
+      t.string     :self_introduction, default: ''
+      t.string     :website,default: ''
+      t.references :user
+      t.timestamps
+    end
+  end
+end
