f59142b4cb221ac3de844cf564fc209efbf71eec
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-02-05 07:29:54 +0900

    Revert "Revert "配列をjson形式に変更""
    This reverts commit fc9acf056d9c57311d79ae8785b912b9bcaf8a74.

---
+++ app/javascript/packs/book-shelf-tab.js
@@ -0,0 +1,8 @@
+import ReactOnRails from 'react-on-rails';
+
+import BookShelfTab from '../bundles/HelloWorld/components/BookShelfTab';
+
+// This is how react_on_rails can see the HelloWorld in the browser.
+ReactOnRails.register({
+  BookShelfTab,
+});
